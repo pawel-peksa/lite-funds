145bca38f99b640aaf43a64a36ac07ddfb972cfa
Paweł <pawelpeksa@gmail.com>
2021-12-09 16:24:38 +0100

    add Dashboard

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { SignIn } from "./pages/SignIn";
 import { SignUp } from "./pages/SignUp";
+import { Dashboard } from "./pages/Dashboard";
 import CssBaseline from "@mui/material/CssBaseline";
 import { customTheme } from "./settings/customTheme";
 import { ThemeProvider } from "@mui/material/styles";
@@ -10,9 +11,10 @@ import { SignInSignUpContainer } from "./components/SignInSignUpContainer";
 const App = () => {
   return (
     <Router>
-      <CssBaseline />
       <ThemeProvider theme={customTheme}>
+        <CssBaseline />
         <Routes>
+          <Route path="/" element={<Dashboard />} />
           <Route element={<SignInSignUpContainer />}>
             <Route path="/sign-in" element={<SignIn />} />
             <Route path="/sign-up" element={<SignUp />} />
---
+++ src/components/SideNav.js
@@ -0,0 +1,116 @@
+import {
+  Drawer,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  Divider,
+} from "@mui/material";
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
+import PaidOutlinedIcon from "@mui/icons-material/PaidOutlined";
+import HistoryIcon from "@mui/icons-material/History";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import DonutSmallOutlinedIcon from "@mui/icons-material/DonutSmallOutlined";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+
+export const SideNav = () => {
+  const drawerWidth = 70;
+
+  const icons = [
+    <AccountBalanceWalletOutlinedIcon />,
+    <PaidOutlinedIcon />,
+    <HistoryIcon />,
+    <DonutSmallOutlinedIcon />,
+    <TrendingUpIcon
+      sx={{ borderBottom: 2, borderLeft: 2, borderColor: "primary.main" }}
+    />,
+  ];
+  return (
+    <Drawer
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        // "& .MuiDrawer-paper": {
+        //   width: drawerWidth,
+        //   boxSizing: "border-box",
+        // },
+      }}
+      variant="permanent"
+      anchor="left"
+    >
+      <Typography
+        variant="body1"
+        component="h1"
+        align="center"
+        color="primary.main"
+        sx={{ mt: 3, pt: 1, pb: 1, mb: 1, fontWeight: 500 }}
+      >
+        Lite Funds
+      </Typography>
+      <List>
+        {/* <Divider component="li" /> */}
+
+        {["Portfolio", "Transaction", "History", "Wallets", "Market"].map(
+          (text, index) => (
+            <ListItemButton
+              key={text}
+              sx={{
+                flexDirection: "column",
+              }}
+            >
+              <ListItemIcon
+                sx={{ justifyContent: "center", color: "primary.main" }}
+              >
+                {icons[index]}
+              </ListItemIcon>
+
+              <ListItemText
+                primary={text}
+                primaryTypographyProps={{ fontSize: 13, fontWeight: "medium" }}
+              />
+            </ListItemButton>
+          )
+        )}
+        {/* <Divider component="li" /> */}
+      </List>
+      <List sx={{ mt: "auto" }}>
+        <Divider component="li" />
+
+        <ListItemButton
+          sx={{
+            flexDirection: "column",
+          }}
+        >
+          <ListItemIcon
+            sx={{ justifyContent: "center", color: "primary.main" }}
+          >
+            <AccountCircleIcon fontSize="large" />
+          </ListItemIcon>
+
+          <ListItemText
+            primary="Account"
+            primaryTypographyProps={{ fontSize: 13, fontWeight: "medium" }}
+          />
+        </ListItemButton>
+        <ListItemButton
+          sx={{
+            flexDirection: "column",
+          }}
+        >
+          <ListItemIcon
+            sx={{ justifyContent: "center", color: "primary.main" }}
+          >
+            <LogoutOutlinedIcon />
+          </ListItemIcon>
+
+          <ListItemText
+            primary="Log out"
+            primaryTypographyProps={{ fontSize: 13, fontWeight: "medium" }}
+          />
+        </ListItemButton>
+      </List>
+    </Drawer>
+  );
+};
