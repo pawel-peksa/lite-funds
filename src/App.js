8f8820d9de71848893f5148ddb455d0104925e20
Paweł <pawelpeksa@gmail.com>
2021-12-13 14:55:50 +0100

    add styles.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./settings/firebaseConfig";
+import "./styles.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { SignIn } from "./pages/SignIn";
 import { SignUp } from "./pages/SignUp";
