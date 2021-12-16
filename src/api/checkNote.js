0012eead3c6b011a0344eabc9565d314a60841e2
Paweł <pawelpeksa@gmail.com>
2021-12-16 23:29:34 +0100

    check if 5 api calls exceeded

---
+++ src/api/checkNote.js
@@ -0,0 +1,10 @@
+const note = {
+  Note: "Thank you for using Alpha Vantage! Our standard API call frequency is 5 calls per minute and 500 calls per day. Please visit https://www.alphavantage.co/premium/ if you would like to target a higher API call frequency.",
+};
+
+export const checkNote = (data) => {
+  if (data["Note"] === note["Note"])
+    alert(
+      "Our data provider limits data calls frequency to 5 calls per minute. But don't worry, You can probably try again already ;)"
+    );
+};
